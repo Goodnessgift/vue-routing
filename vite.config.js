82979e299001d2d439d54da983132ad898eb5f9a
Goodnessgift <141639049+Goodnessgift@users.noreply.github.com>
2024-07-29 09:47:00 +0100

    updated build

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  base:'/vue-routing/',
+  build:{
+    outDir: 'docs', //Ensure this points to the correct output directory
+    assetsDir: 'assets'
   }
 })
