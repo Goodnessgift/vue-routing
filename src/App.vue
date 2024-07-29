3512732f69d5baf14d76b82060771d7d3e24b259
Goodnessgift <141639049+Goodnessgift@users.noreply.github.com>
2024-07-29 09:59:42 +0100

    updated build

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,32 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
+
   <body>
     <RouterView></RouterView>
-    </body>
+  </body>
 </template>
 
 <style>
-  .body{
-    height: 100vh;
-    display: flex;
-    place-items: center;
-  }
-.container{
-    display: flex;
-    gap: 10px;
-    place-items: center;
-    
+body {
+  height: 100vh;
+  display: flex;
+  place-items: center;
 }
-.btn{
-    padding: 10px;
-    /*background-color: aquamarine;
+
+.container {
+  display: flex;
+  gap: 10px;
+  place-items: center;
+
+}
+
+.btn {
+  padding: 10px;
+  /*background-color: aquamarine;
     color:black;*/
-    border-radius: 10px;
-    cursor: pointer;
-    
-  }
+  border-radius: 10px;
+  cursor: pointer;
 
-
+}
 </style>
