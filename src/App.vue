a08185e05f8e1254a1d07e8181c6bdb923e80823
Goodnessgift <141639049+Goodnessgift@users.noreply.github.com>
2024-07-29 09:56:01 +0100

    updated

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,17 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <RouterView></RouterView>
+  <body>
+    <RouterView></RouterView>
+    </body>
 </template>
 
 <style>
-  
+  .body{
+    height: 100vh;
+    display: flex;
+    place-items: center;
+  }
 .container{
     display: flex;
     gap: 10px;
